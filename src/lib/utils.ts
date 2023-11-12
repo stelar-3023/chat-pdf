63850b69a32d1ac675d2937cd920194e7b034beb
Steve Larsen <slarsen-3@att.net>
2023-11-12 13:34:04 -0600

    convert to ascii function

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,10 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+ // function to convert to ascii
+ export function convertToAscii(inputString: string) {
+// remove any non ascii characters
+const asciiString = inputString.replace(/[^\x00-\x7F]/g, "");
+return asciiString;
+}
