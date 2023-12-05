fd2dcb174e0a270951981f0226bbd64d24856634
Steve Larsen <slarsen-3@att.net>
2023-12-05 15:54:47 -0600

    added styling to ChatComponent

--- src/components/ChatComponent.tsx
+++ src/components/ChatComponent.tsx
@@ -1,12 +1,43 @@
 'use client';
-import React from 'react'
+import React from 'react';
+import { Input } from './ui/input';
+import { useChat } from 'ai/react';
+import { Button } from './ui/button';
+import { Send } from 'lucide-react';
+import MessageList from './MessageList';
 
-type Props = {}
+type Props = {};
 
 const ChatComponent = (props: Props) => {
+  const { input, handleInputChange, handleSubmit, messages } = useChat({
+    api: '/api/chat',
+  });
   return (
-    <div>ChatComponent</div>
-  )
-}
+    <div className='relative max-h-screen overflow-scroll'>
+      {/* header */}
+      <div className='sticky top-0 inset-x-0 p-2 bg-white h-fit'>
+        <h3 className='text-xl font-bold'>Chat</h3>
+      </div>
 
-export default ChatComponent
+      {/* message list */}
+      <MessageList messages={messages} />
+
+      <form
+        onSubmit={handleSubmit}
+        className='sticky bottom-0 inset-x-0 px-2 py-4 bg-white'
+      >
+        <Input
+          value={input}
+          onChange={handleInputChange}
+          placeholder='Message AI...'
+          className='w-full'
+        />
+        <Button className='bg-blue-600 mt-2'>
+          <Send className='h-4 w-4' />
+        </Button>
+      </form>
+    </div>
+  );
+};
+
+export default ChatComponent;
